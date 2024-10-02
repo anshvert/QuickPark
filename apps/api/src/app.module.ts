3284167de755ada0c130437f61cae0aefa9f17c2
ANSH <anshtyagi2222@gmail.com>
2024-10-02 21:33:25 +0530

    Graphql init

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import * as process from "node:process";
+import { join } from 'node:path';
+import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            introspection: true,
+            fieldResolverEnhancers: ['guards'],
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            buildSchemaOptions: {
+                numberScalarMode: 'integer'
+            }
+        }),
+        AppModule,
+        UsersModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 
 export class AppModule {}
